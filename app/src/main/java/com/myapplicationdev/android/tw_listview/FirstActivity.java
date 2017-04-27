dd7beb0429d838b2ba2ef65c84fa4f59b92259b4
15039840 <15039840@myrp.edu.sg>
2017-04-27 14:21:31 +0800

    P02

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,16 +1,21 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity {
 
     ListView lv;
-    ArrayList<String> al;
+    TextView tv;
+    ArrayList<String> modules;
     ArrayAdapter<String> aa;
 
     @Override
@@ -18,9 +23,27 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = (ListView) this.findViewById(R.id.lvModule);
+        tv = (TextView) this.findViewById(R.id.tvYear);
 
-        al = new ArrayList<String>();
-        al.add("Year 1");
+
+        modules = new ArrayList<String>();
+        modules.add("Year 1");
+        modules.add("Year 2");
+        modules.add("Year 3");
+
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, modules);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String year = modules.get(position).toString();
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+                i.putExtra("year", year);
+                startActivity(i);
+            }
+        });
 
 
 
--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -2,11 +2,57 @@ package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
 
-public class ModuleAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+/**
+ * Created by 15039840 on 27/4/2017.
+ */
+
+public class ModuleAdapter extends ArrayAdapter<Module> {
+    private ArrayList<Module> module;
     private Context context;
+    private TextView tvProgName;
+    private ImageView ivStar;
 
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects){
+        super(context, resource, objects);
+        // Store the food that is passed to this adapter
+        module = objects;
+        // Store Context object as we would need to use it later
+        this.context = context;
+    }
 
+    // getView() is the method ListView will call to get the
+    //  View object every time ListView needs a row
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to
+        //  "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.row, parent, false);
 
+        // Get the TextView object
+        tvProgName = (TextView) rowView.findViewById(R.id.tvYear);
+
+        // The parameter "position" is the index of the
+        //  row ListView is requesting.
+        //  We get back the food at the same index.
+        Module currentFood = module.get(position);
+        // Set the TextView to show the food
+
+        tvProgName.setText(currentFood.getModule());
+        // Return the nicely done up View to the ListView
+        return rowView;
+    }
 
 }
